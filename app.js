911f22893875d7d8385bc4a4abba0ac98fbacac2
Luken <67332829+Lukendziur@users.noreply.github.com>
2021-02-22 21:15:55 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
-let tareas = require('./funcionesDeTareas'); 
+let funcionalidadTareas = require('./funcionalidadTareas');
 
-let accion = process.argv[2];
+let accion = process.argv[2]; 
+let tituloTerminal = process.argv[3];
+let estadoTerminal = process.argv[4];
+let filtroEstado = process.argv[3];
 
-switch(accion) {
+
+
+switch (accion) {
     case 'listar':
-      tareas.listar() 
+       funcionalidadTareas.listar();
+
         break;
-    case 'agregar':
-        console.log('Agregando...')
-        tareas.agregar();
+    case 'crear':
+        funcionalidadTareas.crear(tituloTerminal, estadoTerminal);
         
+        break; 
+    case 'filtrar':
+        funcionalidadTareas.filtrar(filtroEstado);
+        
+        break;  
     case undefined:
-        console.log('Tenés que pasarme una acción');
-        break;
+        console.log('Debes darme una instrucción');
+        break; 
 
     default:
-       console.log('No entiendo qué me estás pidiendo');
-       console.log('Las acciones disponibles son: listar');
+        console.log('Las opciones disponibles son: listar, filtrar y crear');
         break;
 }
-
-
-
-
